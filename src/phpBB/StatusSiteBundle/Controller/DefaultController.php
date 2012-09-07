b4ebdae3c2de37d16c8205d8a8b945fbf7da9111
Unknown Bliss <m@michaelcullum.com>
2012-09-07 21:01:33 +0100

    Re-doing stuff

--- src/phpBB/StatusSiteBundle/Controller/DefaultController.php
+++ src/phpBB/StatusSiteBundle/Controller/DefaultController.php
@@ -3,29 +3,43 @@
 namespace phpBB\StatusSiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller,
-	phpBB\StatusSiteBundle\StatusInformation,
-	phpBB\StatusSiteBundle\Updates,
-	phpBB\StatusSiteBundle\Checks,
-	phpBB\StatusSiteBundle\User;
+	phpBB\StatusSiteBundle\Entity\Status,
+	phpBB\StatusSiteBundle\Entity\Updates;
+	//phpBB\StatusSiteBundle\Checks,
+	//phpBB\StatusSiteBundle\User;
 
 class DefaultController extends Controller
 {
 	public function indexAction()
 	{
-		$status = new StatusInformation();
+		$status = $this	->getDoctrine()
+						->getRepository('phpBBStatusSiteBundle:Status');
 
-		$updates = new Updates();
-		$updates->set_numberUpdates(7);
+		$updates = $this	->getDoctrine()
+							->getRepository('phpBBStatusSiteBundle:Updates');
 
-		$checks = new Checks();
+		//$checks = new Checks();
 
-		$user = new User();
+		//$user = new User();
+		//
+		$overall_status = $status->findOneByName('overall');
+		$last_check = $status->findOneByName('last_check');
+
+		if (!$overall_status)
+		{
+			$overall_status = "Fully Operational";
+		}
+
+		if (!$last_check) 
+		{
+			$last_check = time();
+		}
 
 		return $this->render('phpBBStatusSiteBundle:Default:index.html.twig', array(
-			'status'		=> $status->get_overall("String"), // Minor Outage, Major Outage or Fully Operational
-			'last_check'	=> $status->get_last_check(), // Datetime of last check
-			'updates'		=> $updates->get_updates(), // Array of updates (number of updates = Ln 18). Each top level element of the array contains all information about that update.
-			'checks'		=> $checks->get_list(), // List of checks with brief information on 2nd level.
+			'status'		=> $overall_status, // Minor Outage, Major Outage or Fully Operational
+			'last_check'	=> $last_check,// Datetime of last check
+			'updates'		=> $updates->findAll(array('post_time' => 'DEC')), // Array of updates (). Each top level element of the array contains all information about that update.
+			//'checks'		=> $checks->get_list(), // List of checks with brief information on 2nd level.
 		));
 	}
 }
