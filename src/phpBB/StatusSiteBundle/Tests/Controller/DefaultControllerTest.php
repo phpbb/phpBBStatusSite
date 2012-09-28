72229401091c045219b77c47c53a30a1337691a1
Unknown Bliss <m@michaelcullum.com>
2012-09-28 18:32:38 +0100

    Commiting the bundle proper

---
+++ src/phpBB/StatusSiteBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace phpBB\StatusSiteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
---
+++ src/phpBB/StatusSiteBundle/phpBBStatusSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace phpBB\StatusSiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class phpBBStatusSiteBundle extends Bundle
+{
+}
