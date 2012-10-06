4739d6fd27e84967d55a9fa8ed695c6832db115c
Unknown Bliss <m@michaelcullum.com>
2012-10-06 22:47:08 +0100

    Some other bits and bobs

--- src/phpBB/StatusSiteBundle/DependencyInjection/Configuration.php
+++ src/phpBB/StatusSiteBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('php_bb_status_site');
+        $rootNode = $treeBuilder->root('phpbb_status_site');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
