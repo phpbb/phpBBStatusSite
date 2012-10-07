14894835a1d586bd95a444db1d34fb3872c3dca0
Unknown Bliss <m@michaelcullum.com>
2012-10-07 19:00:55 +0100

    Weee some sexy calling of classes and erm... thats it?

--- src/phpBB/StatusSiteBundle/Controller/DefaultController.php
+++ src/phpBB/StatusSiteBundle/Controller/DefaultController.php
@@ -2,12 +2,30 @@
 
 namespace phpBB\StatusSiteBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+	phpBB\StatusSiteBundle\StatusInformation,
+	phpBB\StatusSiteBundle\Updates,
+	phpBB\StatusSiteBundle\Checks,
+	phpBB\StatusSiteBundle\User;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('phpBBStatusSiteBundle:Default:index.html.twig');
-    }
+	public function indexAction()
+	{
+		$status = new StatusInformation();
+
+		$updates = new Updates();
+		$updates->set_numberUpdates(7);
+
+		$checks = new Checks();
+
+		$user = new User();
+
+		return $this->render('phpBBStatusSiteBundle:Default:index.html.twig', array(
+			'status'		=> $status->get_overall("String"), // Minor Outage, Major Outage or Fully Operational
+			'last_check'	=> $status->get_last_check(), // Datetime of last check
+			'updates'		=> $updates->get_updates(), // Array of updates (number of updates = Ln 18). Each top level element of the array contains all information about that update.
+			'checks'		=> $checks->get_list(), // List of checks with brief information on 2nd level.
+		));
+	}
 }
