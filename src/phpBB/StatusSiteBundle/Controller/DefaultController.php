d8fa0cc8ed0552324f159e1ef088968827ce00bd
Unknown Bliss <m@michaelcullum.com>
2012-11-09 10:33:49 +0100

    stuff for paul

--- src/phpBB/StatusSiteBundle/Controller/DefaultController.php
+++ src/phpBB/StatusSiteBundle/Controller/DefaultController.php
@@ -5,8 +5,6 @@ namespace phpBB\StatusSiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 	phpBB\StatusSiteBundle\Entity\Status,
 	phpBB\StatusSiteBundle\Entity\Updates;
-	//phpBB\StatusSiteBundle\Checks,
-	//phpBB\StatusSiteBundle\User;
 
 class DefaultController extends Controller
 {
@@ -18,28 +16,52 @@ class DefaultController extends Controller
 		$updates = $this	->getDoctrine()
 							->getRepository('phpBBStatusSiteBundle:Updates');
 
-		//$checks = new Checks();
+		$overall_status = ($status->findOneByName('overall')) ?: 'Fully Operational';
+		$last_check = ($status->findOneByName('last_check')) ?: time();
+		$down_type = ($status->findOneByName('DowntimeType')) ?: 0;
 
-		//$user = new User();
-		//
-		$overall_status = $status->findOneByName('overall');
-		$last_check = $status->findOneByName('last_check');
-
-		if (!$overall_status)
+		if ($overall_status == 'Fully Operational')
 		{
-			$overall_status = "Fully Operational";
+			$downtime = 0;
+			$planned = false;
+		}
+		elseif ($overall_status == 'Major Outage' && $down_type != 'Planned') 
+		{
+			$downtime = 1;
+			$planned = false;
+		}
+		elseif ($overall_status == 'Major Outage' && $down_type == 'Planned')
+		{
+			$downtime = 2;
+			$planned = true;
+		}
+		elseif ($overall_status == 'Minor Outage' && $down_type != 'Planned') 
+		{
+			$downtime = 3;
+			$planned = false;
+		}
+		elseif ($overall_status == 'Minor Outage' && $down_type == 'Planned')
+		{
+			$downtime = 4;
+			$planned = true;
 		}
 
-		if (!$last_check) 
+		$updates_all = $updates->findAll();
+		$show = ($status->findOneByName('show_no')) ?: 5;
+
+		for($i = 0, $i == $show, $i++)
 		{
-			$last_check = time();
+			$updates_parsed[$i], $updates_all[$i]
 		}
 
-		return $this->render('phpBBStatusSiteBundle:Default:index.html.twig', array(
+		$template_vars = array(
 			'status'		=> $overall_status, // Minor Outage, Major Outage or Fully Operational
 			'last_check'	=> $last_check,// Datetime of last check
 			'updates'		=> $updates->findAll(array('post_time' => 'DEC')), // Array of updates (). Each top level element of the array contains all information about that update.
 			//'checks'		=> $checks->get_list(), // List of checks with brief information on 2nd level.
-		));
+			'downtime'		=> $downtime, // 0 for up, 1 for
+			'planned'		=> $planned,
+		);
+		return $this->render('phpBBStatusSiteBundle:Default:index.html.twig', $template_vars);
 	}
 }
