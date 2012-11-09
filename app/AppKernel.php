f5176c56620495a18cfac05d75beea1834bcc5ca
Paul Sohier <paul@phpbb.com>
2012-11-09 12:35:52 +0100

    Added sonata admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,10 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new phpBB\StatusSiteBundle\phpBBStatusSiteBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
