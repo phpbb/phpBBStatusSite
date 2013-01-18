713214dac467b79d780aedadbc8f79cc6b02ada1
Unknown Bliss <m@michaelcullum.com>
2013-01-18 21:48:29 +0000

    Add update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+cd phpBBStatusSite/
+git fetch origin
+git reset --hard origin/master
+git pull origin master
+php5-cli composer.phar --no-interaction install --optimize-autoloader
+php5-cli app/console doctrine:migrations:migrate --no-interaction
+php5-cli app/console pingdom:update
+php5-cli app/console cache:clear --env=prod --no-debug
+php5-cli app/console assetic:dump --env=prod --no-debug
+php5-cli app/console cache:warmup
+cd ../
+echo "Complete"
