df0978de214576b27405a1be01ce1bccbef4b63a
MichaelC <m@michaelcullum.com>
2013-08-12 19:08:00 +0100

    Add a general test to check the test framework works

---
+++ src/phpBB/StatusSiteBundle/Tests/GeneralTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace phpBB\StatusSiteBundle\Tests;
+
+class GeneralTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGeneral()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
